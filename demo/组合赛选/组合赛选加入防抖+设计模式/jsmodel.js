aef9ca983fe566ba88ca6e64da5e89d3426f69f1
dylanOcean <836546737@qq.com>
2018-05-31 17:54:45 +0800

    修改jsmodel.js 未完成，思路不清晰， 还需要看一遍视频

---
+++ demo/组合赛选/组合赛选加入防抖+设计模式/jsmodel.js
@@ -0,0 +1,18 @@
+
+
+function getState(prop){
+    return state[prop];
+}
+
+function setState(obj){
+    state[obj.type] = state[obj.value];
+}
+
+function depatch(){
+    
+}
+
+function subscribe(){
+
+}
+
--- demo/组合赛选/组合赛选加入防抖+设计模式/unShake.js
+++ demo/组合赛选/组合赛选加入防抖+设计模式/unShake.js
@@ -1,6 +1,8 @@
 var deShake =  (function(){
     var timer = null;
     return  function(handle,delay){
+        var self = this;
+        var arg = this.arguments;
         clearTimeout(timer);
         timer = setTimeout(function(){
             handle();
